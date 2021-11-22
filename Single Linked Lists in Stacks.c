4bf7954141417d2df75642d665a006bd430750cf
harshini-21 <94436588+harshini-21@users.noreply.github.com>
2021-11-22 19:04:59 +0530

    Add files via upload

---
+++ Single Linked Lists in Stacks.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+int data;
+struct node * link;
+struct node * current;
+struct node * temp;
+struct node * top  = NULL;
+};
+void push(int element)
+    current = (struct node *)malloc(size of (struct node));
+    current -> link = NULL;
+if(top == NULL)
+    current -> link = NULL;
+else{
+    current -> link = top;
+    top = current;
+}
+case - 1:
+    printf("Enter element");
+    printf("%d", element)
+    push(element);
+    break;
+case - 2:
+    printf("deleted element is %d");
+    pop();
+    break;
+    }
+case - 3:
+    if(top == NULL)
+    printf("Stack Underflow");
+    else
+    printf("top most element is %d");
+    peek();
+    break;
+case - 4:
+    if(top == NULL)
+    printf("Stack Underflow");
+    else
+        display();
+        break;
+case - 5:
+    exit(0);
+    }
+}
+}
